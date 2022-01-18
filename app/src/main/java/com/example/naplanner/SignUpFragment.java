3f12be375120116d6132702c648cb7487c83f944
Oso <manuelluis736@gmail.com>
2022-01-18 13:57:16 +0100

    Base Sign Up XML

---
+++ app/src/main/java/com/example/naplanner/SignUpFragment.java
@@ -0,0 +1,28 @@
+package com.example.naplanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.naplanner.databinding.FragmentLogInBinding;
+import com.example.naplanner.databinding.FragmentSignUpBinding;
+
+public class SignUpFragment extends Fragment {
+
+    private FragmentSignUpBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSignUpBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+}
