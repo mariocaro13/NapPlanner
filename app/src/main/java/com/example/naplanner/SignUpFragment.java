9e19b4498cf91115de11415fb50414949e196fd2
Oso <manuelluis736@gmail.com>
2022-01-25 13:33:57 +0100

    Added Sign Up Choice Screen, Changed Sign Up Palettes

--- app/src/main/java/com/example/naplanner/SignUpFragment.java
+++ app/src/main/java/com/example/naplanner/SignUpFragment.java
@@ -1,28 +1,25 @@
 package com.example.naplanner;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.example.naplanner.databinding.FragmentLogInBinding;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.example.naplanner.databinding.FragmentSignUpBinding;
 import com.example.naplanner.model.UserModel;
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.Locale;
 
 public class SignUpFragment extends Fragment {
 
     private FragmentSignUpBinding binding;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,10 +32,10 @@ public class SignUpFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setupUI();
+        swapPalette(SignUpFragmentArgs.fromBundle(getArguments()).getUserChoice());
     }
 
-
-    private void setupUI(){
+    private void setupUI() {
         binding.signUpFragmentSendButton.setOnClickListener(new View.OnClickListener() {
             UserModel data = new UserModel();
 
@@ -46,7 +43,7 @@ public class SignUpFragment extends Fragment {
             public void onClick(View view) {
 
                 String username = binding.signUpFragmentFormLayout.signUpFragmentUsernameEditText.getText().toString();
-                if(!username.isEmpty())
+                if (!username.isEmpty())
                     data.setUsername(username);
                 else {
                     sendErrorMsg("El Nombre de usuario esta vacio");
@@ -54,7 +51,7 @@ public class SignUpFragment extends Fragment {
                 }
 
                 String mail = binding.signUpFragmentFormLayout.signUpFragmentMailEditText.getText().toString();
-                if(validateEmail(mail))
+                if (validateEmail(mail))
                     data.setMail(mail);
                 else {
                     sendErrorMsg("Por favor introduzca un email valido");
@@ -63,11 +60,11 @@ public class SignUpFragment extends Fragment {
 
                 String pass = binding.signUpFragmentFormLayout.signUpFragmentPassEditText.getText().toString();
                 String conPass = binding.signUpFragmentFormLayout.signUpFragmentConfirmPassEditText.getText().toString();
-                if(pass.isEmpty())
+                if (pass.isEmpty())
                     sendErrorMsg("Introduzca una contraseña");
-                else if(pass.equals(conPass))
+                else if (pass.equals(conPass))
                     data.setPass(pass);
-                else{
+                else {
                     sendErrorMsg("Las contraseñas no son iguales");
                 }
 
@@ -82,11 +79,36 @@ public class SignUpFragment extends Fragment {
         });
     }
 
-    private boolean validateEmail(CharSequence mail){
-        return  !mail.toString().isEmpty() && Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+    private void swapPalette(int choice) {
+        if (choice == 2)
+            useStudentPalette();
     }
 
-    private void sendErrorMsg(String error){
+    private void useStudentPalette() {
+        int bgColorID = getResources().getColor(R.color.background_green, null);
+        int lightColorID = getResources().getColor(R.color.light_green, null);
+        int darkColorID = getResources().getColor(R.color.dark_green, null);
+        binding.getRoot().setBackgroundColor(bgColorID);
+
+        binding.signUpFragmentFormLayout.signUpFragmentUsernameTextView.setBackgroundColor(darkColorID);
+        binding.signUpFragmentFormLayout.signUpFragmentMailTextView.setBackgroundColor(darkColorID);
+        binding.signUpFragmentFormLayout.signUpFragmentPassTextView.setBackgroundColor(darkColorID);
+        binding.signUpFragmentFormLayout.signUpFragmentConfirmPassTextView.setBackgroundColor(darkColorID);
+
+        binding.signUpFragmentSendButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.dark_green));
+
+        binding.signUpFragmentFormLayout.signUpFragmentUsernameEditText.setBackgroundColor(lightColorID);
+        binding.signUpFragmentFormLayout.signUpFragmentMailEditText.setBackgroundColor(lightColorID);
+        binding.signUpFragmentFormLayout.signUpFragmentPassEditText.setBackgroundColor(lightColorID);
+        binding.signUpFragmentFormLayout.signUpFragmentConfirmPassEditText.setBackgroundColor(lightColorID);
+
+    }
+
+    private boolean validateEmail(CharSequence mail) {
+        return !mail.toString().isEmpty() && Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+    }
+
+    private void sendErrorMsg(String error) {
         Toast.makeText(requireActivity().getApplicationContext(), error, Toast.LENGTH_SHORT).show();
     }
 }
