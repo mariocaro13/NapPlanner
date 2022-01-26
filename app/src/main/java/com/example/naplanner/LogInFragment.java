731b8d25a9bda133b0c9ad70a92fb83d23121b62
Oso <manuelluis736@gmail.com>
2022-01-26 13:27:07 +0100

    Sync

--- app/src/main/java/com/example/naplanner/LogInFragment.java
+++ app/src/main/java/com/example/naplanner/LogInFragment.java
@@ -1,15 +1,18 @@
 package com.example.naplanner;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.naplanner.databinding.FragmentLogInBinding;
+import com.example.naplanner.model.UserModel;
 
 public class LogInFragment extends Fragment {
 
@@ -25,14 +28,52 @@ public class LogInFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setupUI();
+
+    }
+
+    private void setupUI() {
+        binding.logInFragmentSendButton.setOnClickListener(new View.OnClickListener() {
+            UserModel data = new UserModel();
+
+            @Override
+            public void onClick(View view) {
+
+                String input = binding.logInFragmentMailEditText.getText().toString();
 
 
+                if (!input.isEmpty() && validateEmail(input)) {
+                    data.setMail(input);
+                } else if (!input.isEmpty()) {
+                    data.setUsername(input);
+                } else {
+                    sendErrorMsg("El Nombre de usuario esta vacio");
+                    return;
+                }
+
+                String pass = binding.logInFragmentPasswordEditText.getText().toString();
+                if (!pass.isEmpty())
+                    data.setPass(pass);
+                else {
+                    sendErrorMsg("Introduzca una contraseña");
+                }
+
+            }
+        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private boolean validateEmail(CharSequence mail) {
+        return !mail.toString().isEmpty() && Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+    }
+
+    private void sendErrorMsg(String error) {
+        Toast.makeText(requireActivity().getApplicationContext(), error, Toast.LENGTH_SHORT).show();
     }
 
 }
