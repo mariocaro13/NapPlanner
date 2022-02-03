dbac0f408498ff55b6a42a81e0d5b85c011f6227
mariocaro13 <mariocaro2002@gmail.com>
2022-02-03 14:12:02 +0100

    Task form fragment

---
+++ app/src/main/java/com/example/naplanner/features/main/TaskForm.java
@@ -0,0 +1,27 @@
+package com.example.naplanner.features.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.naplanner.R;
+
+public class TaskForm extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_form, container, false);
+    }
+}
