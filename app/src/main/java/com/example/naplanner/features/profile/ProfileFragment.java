51da5a6b7b82b0ab44788e83f7935089c5e4b9ee
Mario <mariocaro2002@gmail.com>
2022-02-14 19:59:16 +0100

    Profile, not done count of completed tasks

--- app/src/main/java/com/example/naplanner/features/profile/ProfileFragment.java
+++ app/src/main/java/com/example/naplanner/features/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.naplanner.features.profile;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,23 @@ import com.example.naplanner.MainActivity;
 import com.example.naplanner.R;
 import com.example.naplanner.databinding.FragmentProfileBinding;
 import com.example.naplanner.features.main.TaskFormFragmentArgs;
+import com.example.naplanner.helperclasses.Constants;
 import com.example.naplanner.model.TaskModel;
+import com.example.naplanner.model.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
-
+    private int tasksCompleteCount = 0;
     private FragmentProfileBinding binding;
+    private FirebaseAuth fAuth;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,6 +43,8 @@ public class ProfileFragment extends Fragment {
     public void onStart() {
         super.onStart();
         ((MainActivity) requireActivity()).hideInteractionBars();
+        fAuth = FirebaseAuth.getInstance();
+        setupUI();
     }
 
     @Override
@@ -41,6 +56,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         binding.profileFragmentBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,9 +67,47 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(requireView()).navigate(R.id.action_profileFragment_to_FirstFragment);
-                
+
             }
         });
     }
 
+    public void setupUI() {
+
+        binding.profileFragmentUserMailTextView.setText(Objects.requireNonNull(fAuth.getCurrentUser()).getEmail());
+        FirebaseDatabase.getInstance(Constants.databaseURL).getReference().child("User").child(Objects.requireNonNull(fAuth.getCurrentUser()).getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                binding.profileFragmentUserNameTextView.setText(snapshot.getValue(UserModel.class).getUsername());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        countCompleteTasks();
+    }
+
+    public  void countCompleteTasks() {
+        FirebaseDatabase.getInstance(Constants.databaseURL).getReference().child("Tasks").child(Objects.requireNonNull(fAuth.getCurrentUser()).getUid()).addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if(Objects.requireNonNull(dataSnapshot.getValue(TaskModel.class)).isCompleteTask()){
+                        tasksCompleteCount ++;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        binding.profileFragmentTasksCountTextView.setText(""+ tasksCompleteCount);
+
+    }
 }
