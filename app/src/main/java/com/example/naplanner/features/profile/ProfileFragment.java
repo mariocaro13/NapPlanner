69b2169fea50916f4a16b510fab0f1426efb6195
UnOso <manuelluis736@gmail.com>
2022-04-30 15:10:51 +0200

    Upload Selected Image

--- app/src/main/java/com/example/naplanner/features/profile/ProfileFragment.java
+++ app/src/main/java/com/example/naplanner/features/profile/ProfileFragment.java
@@ -2,15 +2,19 @@ package com.example.naplanner.features.profile;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -22,21 +26,23 @@ import com.example.naplanner.databinding.FragmentProfileBinding;
 import com.example.naplanner.helperclasses.Constants;
 import com.example.naplanner.model.TaskModel;
 import com.example.naplanner.model.UserModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
-    private FirebaseAuth fAuth;
+    private FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    private FirebaseStorage fStorage = FirebaseStorage.getInstance();
     private int tasksCompleteCount;
+    private ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), UploadSelectedImage());
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -48,7 +54,6 @@ public class ProfileFragment extends Fragment {
     public void onStart() {
         super.onStart();
         ((MainActivity) requireActivity()).hideInteractionBars();
-        fAuth = FirebaseAuth.getInstance();
         setupUI();
     }
 
@@ -63,12 +68,9 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         binding.profileFragmentBackButton.setOnClickListener(view1 -> Navigation.findNavController(requireView()).navigateUp());
-        binding.profileFragmentLogOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fAuth.signOut();
-                Navigation.findNavController(requireView()).navigate(R.id.action_profileFragment_to_LoginFragment);
-            }
+        binding.profileFragmentLogOutButton.setOnClickListener(v -> {
+            fAuth.signOut();
+            Navigation.findNavController(requireView()).navigate(R.id.action_profileFragment_to_LoginFragment);
         });
     }
 
@@ -78,6 +80,7 @@ public class ProfileFragment extends Fragment {
 
         binding.profileFragmentUserMailTextView.setText(Objects.requireNonNull(fAuth.getCurrentUser()).getEmail());
         binding.fragmentProfileResetPasswordTextView.setOnClickListener(updatePassword());
+        binding.profileFragmentAppIconImageView.setOnClickListener(selectAndUploadImage());
         FirebaseDatabase.getInstance(Constants.databaseURL).getReference().child("User").child(Objects.requireNonNull(fAuth.getCurrentUser()).getUid()).addValueEventListener(new ValueEventListener() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -119,39 +122,44 @@ public class ProfileFragment extends Fragment {
     }
 
     private View.OnClickListener updatePassword() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText newPassword = new EditText(view.getContext());
-                newPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                AlertDialog.Builder passwordReset = new AlertDialog.Builder(view.getContext());
-                passwordReset.setTitle("Cambiar Contraseña?");
-                passwordReset.setMessage("Introduzca una nueva contraseña (minimo 6 de longitud)");
-                passwordReset.setView(newPassword);
+        return view -> {
+            EditText newPassword = new EditText(view.getContext());
+            newPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            AlertDialog.Builder passwordReset = new AlertDialog.Builder(view.getContext());
+            passwordReset.setTitle("Cambiar Contraseña?");
+            passwordReset.setMessage("Introduzca una nueva contraseña (minimo 6 de longitud)");
+            passwordReset.setView(newPassword);
 
-                passwordReset.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String password = newPassword.getText().toString();
-                        Objects.requireNonNull(fAuth.getCurrentUser()).updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful())
-                                    Toast.makeText(ProfileFragment.this.getContext(), "Contraseña Cambiada Correctamente", Toast.LENGTH_SHORT).show();
-                                else
-                                    Toast.makeText(ProfileFragment.this.getContext(), "Un Error Ha Ocurrido", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
+            passwordReset.setPositiveButton("Confirmar", (dialogInterface, i) -> {
+                String password = newPassword.getText().toString();
+                Objects.requireNonNull(fAuth.getCurrentUser()).updatePassword(password).addOnCompleteListener(task -> {
+                    if (task.isSuccessful())
+                        Toast.makeText(ProfileFragment.this.getContext(), "Contraseña Cambiada Correctamente", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(ProfileFragment.this.getContext(), "Un Error Ha Ocurrido", Toast.LENGTH_SHORT).show();
                 });
-                passwordReset.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                    }
-                });
+            });
+            passwordReset.setNegativeButton("Cancelar", (dialogInterface, i) -> {
+            });
 
-                passwordReset.create().show();
-            }
+            passwordReset.create().show();
         };
+    }
+
+
+    private ActivityResultCallback<Uri> UploadSelectedImage(){
+        return uri -> {
+            UploadTask uploadTask = fStorage.getReference().child("/users/" + Objects.requireNonNull(fAuth.getCurrentUser()).getUid()).putFile(uri);
+            uploadTask.addOnSuccessListener(taskSnapshot -> {
+                Toast.makeText(ProfileFragment.this.getContext(), "Imagen Subida Correctamente", Toast.LENGTH_SHORT).show();
+            }).addOnFailureListener(e -> {
+                Toast.makeText(ProfileFragment.this.getContext(), "Fallo en la carga de la Imagen", Toast.LENGTH_SHORT).show();
+                Log.d("Image Load Failure: ", e.getMessage());
+            });
+        };
+    }
+
+    private View.OnClickListener selectAndUploadImage() {
+        return view -> mGetContent.launch("image/*");
     }
 }
